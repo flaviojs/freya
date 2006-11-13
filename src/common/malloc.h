bc3390a244cd164d4696312eb9cb39c7559b731e
Harbin <Harbin@a4f22c90-791b-0410-83c5-9a11a2519e46>
2006-11-13 13:57:23 +0000

    Improved: user can now disable memory-operation debugging with --without-debug compiler flag Updated: minor version number (now 3.1.0)
    git-svn-id: http://ookoo.org/svn/freya/subdev@419 a4f22c90-791b-0410-83c5-9a11a2519e46

--- src/common/malloc.h
+++ src/common/malloc.h
@@ -1,18 +1,26 @@
 // $Id: malloc.h 269 2005-09-12 20:41:10Z Yor $
+
 #ifndef _MALLOC_H_
 #define _MALLOC_H_
 
-#include <stdlib.h> // malloc, calloc, free
+#include <stdlib.h>
 
-#define MALLOC(result, type, number) if (!((result) = (type *)malloc((number) * sizeof(type)))) \
-      printf("SYSERR: malloc failure at %s:%d.\n", __FILE__, __LINE__), abort()
+#ifdef __DEBUG
 
-#define CALLOC(result, type, number) if (!((result) = (type *)calloc((number), sizeof(type)))) \
-      printf("SYSERR: calloc failure at %s:%d.\n", __FILE__, __LINE__), abort()
+#define MALLOC(result, type, number) if(!((result) = (type *)malloc((number) * sizeof(type)))) \
+      printf("SYSERR: malloc failure at %s:%d \n", __FILE__, __LINE__), abort()
+#define CALLOC(result, type, number) if(!((result) = (type *)calloc((number), sizeof(type)))) \
+      printf("SYSERR: calloc failure at %s:%d \n", __FILE__, __LINE__), abort()
+#define REALLOC(result, type, number) if(!((result) = (type *)realloc((result), sizeof(type) * (number)))) \
+      printf("SYSERR: realloc failure at %s:%d \n", __FILE__, __LINE__), abort()
+#define FREE(result) if(result) { free(result); result = NULL; } else { printf("SYSERR: free failure at %s:%d \n", __FILE__, __LINE__); }
 
-#define REALLOC(result, type, number) if (!((result) = (type *)realloc((result), sizeof(type) * (number)))) \
-      printf("SYSERR: realloc failure at %s:%d.\n", __FILE__, __LINE__), abort()
+#else
 
-#define FREE(result) if (result) free(result), result = NULL
+#define MALLOC(result, type, number) result = (type *)malloc((number) * sizeof(type));
+#define CALLOC(result, type, number) result = (type *)calloc((number), sizeof(type));
+#define REALLOC(result, type, number) result = (type *)realloc((result), sizeof(type) * (number));
+#define FREE(result) free(result); result = NULL;
 
-#endif // _MALLOC_H_
+#endif /* __DEBUG */
+#endif /* _MALLOC_H_ */
