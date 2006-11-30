f8e1b1d7af5877c98b0836fd921b3c0d4baf93be
Van84 <Van84@a4f22c90-791b-0410-83c5-9a11a2519e46>
2006-11-30 13:25:16 +0000

    fixed a wrong variable in the previous svn
    git-svn-id: http://ookoo.org/svn/freya/subdev@612 a4f22c90-791b-0410-83c5-9a11a2519e46

--- src/map/mob.c
+++ src/map/mob.c
@@ -3142,7 +3142,8 @@ int mob_class_change(struct mob_data *md, int *value, int count)
 	if (count < 1) return 0;
 
 	//added by van84 to check few condition to avoid mob changing class working for nothing
-	if (md->class == class || md->guardian_data) return 0; 
+	if (md->class == class ) return 0; // if the previous class is the same
+	if (md->class >= 1285 && md->class <= 1288) return 0; //emperium and guardian should be ignored
 	if (md->class >= 1324 && md->class <= 1363) return 0; //treasure chest
 
 	i = rand() % count;
