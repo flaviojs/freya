3bc08b935f6da444dbbfb4f94b4bf6fc4367ac6c
Tsuyuki <Tsuyuki@a4f22c90-791b-0410-83c5-9a11a2519e46>
2006-12-13 00:23:08 +0000

    * Fixed: Manteau's job equip IDs [Tsuyuki] * Fixed: Ghostring Card working on mobs [Bison] * Fixed: Completely repaired Kyrie Eleison [Bison] * Improved: the way kyrie eleison blocks and reduces received damage [Harbin]
    git-svn-id: http://ookoo.org/svn/freya/subdev@706 a4f22c90-791b-0410-83c5-9a11a2519e46

--- src/map/battle.c
+++ src/map/battle.c
@@ -542,24 +542,19 @@ int battle_calc_damage(struct block_list *src, struct block_list *bl, int damage
 			}
 		}
 
-		if(tsc_data[SC_KYRIE].timer != -1 && damage > 0)
-		{
-			/* kyrie eleison is able to block damage completely */
-			if(sc_data[SC_KYRIE].val2 > damage)
-			{
-				sc_data[SC_KYRIE].val2 -= damage;
-				if(flag & BF_WEAPON || skill_num == TF_THROWSTONE)
+ if(tsc_data[SC_KYRIE].timer != -1 && damage > 0)
+	{
+		tsc_data[SC_KYRIE].val2 -= damage;
+			if(flag & BF_WEAPON || skill_num == TF_THROWSTONE)
+				{
+				if(tsc_data[SC_KYRIE].val2 >= 0)	
 					damage = 0;
-			/* kyrie eleison is able to block damage partially -> reduce damage and end kyrie eleison */
-			} else {
+				else
+					damage -= tsc_data[SC_KYRIE].val2;
+				}
+			if((--tsc_data[SC_KYRIE].val3) <= 0 || (tsc_data[SC_KYRIE].val2 <= 0) || skill_num == AL_HOLYLIGHT)
 				status_change_end(bl, SC_KYRIE, -1);
-				if(flag & BF_WEAPON || skill_num == TF_THROWSTONE)
-					damage -= sc_data[SC_KYRIE].val2;
 			}
-			/* count hits blocked by kyrie eleison */
-			if((--sc_data[SC_KYRIE].val3) <= 0 || || skill_num == AL_HOLYLIGHT)
-				status_change_end(bl, SC_KYRIE, -1);
-		}
 
 		if (tsc_data[SC_UTSUSEMI].timer != -1)
 		{
@@ -2068,7 +2063,7 @@ struct Damage battle_calc_weapon_attack(struct block_list *src, struct block_lis
 	/* elemental damage fix */
 	if((sd && skill_num != AS_VENOMKNIFE && (skill_num || !battle_config.pc_attack_attr_none)) || (md && (skill_num || !battle_config.mob_attack_attr_none)) || (pd && (skill_num || !battle_config.pet_attack_attr_none)))
 	{
-		if(!(sd && tsd && battle_config.mob_ghostring_fix && t_ele == 8))
+		if(!(!sd && tsd && battle_config.mob_ghostring_fix && t_ele == 8))
 		{
 			short t_element = status_get_element(target);
 			if(wd.damage > 0)
