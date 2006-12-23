174524f7e4d3a32a85e301292cc7c925280ccbda
Bison <Bison@a4f22c90-791b-0410-83c5-9a11a2519e46>
2006-12-23 02:37:26 +0000

    An issue with perfect hide (from pet smokies), MVP/bosses are still allowed to see you regardless.
    git-svn-id: http://ookoo.org/svn/freya/subdev@759 a4f22c90-791b-0410-83c5-9a11a2519e46

--- src/map/mob.c
+++ src/map/mob.c
@@ -705,11 +705,12 @@ static void mob_attack(struct mob_data *md, unsigned int tick, int data)
 	{
 		unlock_target = 1;
 	} else if(tsd) {
-		if(pc_isdead(tsd) || pc_isinvisible(tsd) || tsd->perfect_hiding || tsd->invincible_timer != -1 || tsd->sc_data[SC_TRICKDEAD].timer != -1)
+		if(pc_isdead(tsd) || pc_isinvisible(tsd) || tsd->invincible_timer != -1 || tsd->sc_data[SC_TRICKDEAD].timer != -1)
 			unlock_target = 1;
-		// Corrected MVP behavior.  [Bison]
+		// Corrected MVP behavior.  
 		// Changed the check around a bit to differentiate from MVP mobs vs. non-boss mobs.
-		if(!(mode & 0x20) && (tsd->sc_data[SC_BASILICA].timer != -1 && ((race != 4 || race != 6) && (pc_ishiding(tsd) || pc_iscloaking(tsd) || pc_ischasewalk(tsd) || tsd->state.gangsterparadise))))
+		// Fixed the perfect hide problem with smokies, MVP's are still allowed to see through this effect however.
+		if(!(mode & 0x20) && ((tsd->sc_data[SC_BASILICA].timer != -1 || tsd->perfect_hiding) && ((race != 4 || race != 6) && (pc_ishiding(tsd) || pc_iscloaking(tsd) || pc_ischasewalk(tsd) || tsd->state.gangsterparadise))))
 			unlock_target = 1;
 	}
 
