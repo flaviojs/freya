9cd80f7cb2356ef98f31034f00508c0cb4a0c629
Tsuyuki <Tsuyuki@a4f22c90-791b-0410-83c5-9a11a2519e46>
2007-01-28 21:49:51 +0000

    Oops
    git-svn-id: http://ookoo.org/svn/freya/trunk@1022 a4f22c90-791b-0410-83c5-9a11a2519e46

---
+++ sql/upgrades/r1022.sql
@@ -0,0 +1,10 @@
+# Removed custom mail system - Tsuyuki
+
+# BACKUP YOUR DATABASE BEFORE MAKING ANY CHANGES!
+
+DROP TABLE `mail` 
+# Removed custom mail system - Tsuyuki
+
+# BACKUP YOUR DATABASE BEFORE MAKING ANY CHANGES!
+
+DROP TABLE `mail` 
